f8a74c678b82e3faea715094f4a290a73683d530
Mengjiao Wang <wmjdut@gmail.com>
2015-05-19 15:36:06 +0800

    add simple allocator

--- mystl_alloc.h
+++ mystl_alloc.h
@@ -121,7 +121,35 @@ namespace mystl
     }
   }
 
+
+  // simple allocator
+  template <class T, class Alloc>
+  class simple_alloc
+  {
+    public:
+      static T *allocate(size_t n)
+      {
+        return 0 == n ? 0 : (T*)Alloc::allocate(n*sizeof(T));
+      }
+      static T *allocate(void)
+      {
+        return (T*) Alloc::allocate(sizeof(T));
+      }
+      static void deallocate(T *p, size_t n)
+      {
+        if( n != 0)
+        {
+          Alloc::deallocate(p, n*sizeof(T));
+        }
+      }
+      static void deallocate(T *p)
+      {
+        Alloc::deallocate(p, sizeof(T));
+      }
+  };
+
   typedef __malloc_alloc_template<0> malloc_alloc;
+  typedef malloc_alloc alloc;
 
   // default allocator - implemented with free list
 
