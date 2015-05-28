ec9c497eebbc51fd79ec92a7a1e5a957d4443199
Mengjiao Wang <wmjdut@gmail.com>
2015-05-28 15:44:12 +0800

    add data operation function for deque

--- mystl_deque.h
+++ mystl_deque.h
@@ -117,7 +117,7 @@ struct __deque_iterator
   }
   self &operator+=(difference_type n)
   {
-    difference_type offset = n + (cur + first);
+    difference_type offset = n + (cur - first);
     if(offset >= 0 && offset < difference_type(buffer_size()))
     {
       cur += n;
@@ -172,10 +172,10 @@ inline random_access_iterator_tag
 }
 
 template<class T, class Ref, class Ptr, size_t BufSiz>
-inline typename iterator_traits<T>::value_type*
+inline T*
 value_type(const __deque_iterator<T, Ref, Ptr, BufSiz>&)
 {
-  return static_cast<typename iterator_traits<T>::value_type*>(0);
+  return 0;
 }
 
 template <class T, class Alloc=alloc, size_t BufSiz = 0>
@@ -239,6 +239,82 @@ class deque
         push_front_aux(t);
       }
     }
+
+    void pop_back()
+    {
+      if(finish.cur != finish.first)
+      {
+        --finish.cur;
+        mystl::destroy(finish.cur);
+      }
+      else
+      {
+        pop_back_aux();
+      }
+    }
+
+    void pop_back_aux();
+
+    void pop_front()
+    {
+      if(start.cur != start.last - 1)
+      {
+        destroy(start.cur);
+        ++start.cur;
+      }
+      else
+      {
+        pop_front_aux();
+      }
+    }
+
+    void pop_front_aux();
+
+    void clear();
+
+    iterator erase(iterator pos)
+    {
+      iterator next = pos;
+      ++next;
+      difference_type index = pos - start;
+      if(index < (size() >> 1))
+      {
+        //TODO: will be changed to mystl::copy_backward
+        std::copy_backward(start, pos, next);
+        pop_front();
+      }
+      else
+      {
+        //TODO: will be changed to mystl::copy
+        std::copy(next, finish, pos);
+        pop_back();
+      }
+      return start + index;
+    }
+
+    iterator erase(iterator first, iterator last);
+
+    iterator insert(iterator position, const value_type &x)
+    {
+      if(position.cur == start.cur)
+      {
+        push_front(x);
+        return start;
+      }
+      else if(position.cur == finish.cur)
+      {
+        push_back(x);
+        iterator tmp = finish;
+        --tmp;
+        return tmp;
+      }
+      else
+      {
+        return insert_aux(position, x);
+      }
+    }
+
+    iterator insert_aux(iterator position, const value_type &x);
 
   private:
     void fill_initialize(size_type n, const value_type &value);
@@ -378,6 +454,27 @@ void deque<T, Alloc, BufSize>::push_front_aux(const value_type &t)
 }
 
 template <class T, class Alloc, size_t BufSize>
+void deque<T, Alloc, BufSize>::clear()
+{
+  for(map_pointer node = start.node + 1; node < finish.node; ++node)
+  {
+    mystl::destroy(*node, *node + buffer_size());
+    data_allocator::deallocate(*node, buffer_size());
+  }
+  if(start.node != finish.node)
+  {
+    destroy(start.cur, start.last);
+    destroy(finish.first, finish.cur);
+    data_allocator::deallocate(finish.first, buffer_size());
+  }
+  else
+  {
+    destroy(start.cur, finish.cur);
+  }
+  finish = start;
+}
+
+template <class T, class Alloc, size_t BufSize>
 void deque<T, Alloc, BufSize>::reallocate_map(size_type nodes_to_add, bool add_at_front)
 {
   size_type old_num_nodes = finish.node - start.node + 1;
@@ -413,6 +510,98 @@ void deque<T, Alloc, BufSize>::reallocate_map(size_type nodes_to_add, bool add_a
   }
   start.set_node(new_nstart);
   finish.set_node(new_nstart + old_num_nodes - 1);
+}
+
+template <class T, class Alloc, size_t BufSize>
+void deque<T, Alloc, BufSize>::pop_back_aux()
+{
+  deallocate_node(finish.first);
+  finish.set_node(finish.node - 1);
+  finish.cur = finish.last - 1;
+  destroy(finish.cur);
+}
+
+template <class T, class Alloc, size_t BufSize>
+void deque<T, Alloc, BufSize>::pop_front_aux()
+{
+  deallocate_node(start.first);
+  start.set_node(start.node + 1);
+  start.cur = start.first;
+}
+
+template <class T, class Alloc, size_t BufSize>
+typename deque<T, Alloc, BufSize>::iterator
+deque<T, Alloc, BufSize>::erase(iterator first, iterator last)
+{
+  if(first == start && last == finish)
+  {
+    clear();
+    return finish;
+  }
+  else
+  {
+    difference_type n = last - first;
+    difference_type elems_before = first - start;
+    if(elems_before < (size() - n) / 2)
+    {
+      //TODO: will be changed to mystl::copy_backward
+      std::copy_backward(start, first, last);
+      iterator new_start = start + n;
+      mystl::destroy(start, new_start);
+      for(map_pointer cur = start.node; cur < new_start.node; ++cur)
+      {
+        data_allocator::deallocate(*cur, buffer_size());
+      }
+      start = new_start;
+    }
+    else
+    {
+      //TODO: will be changed to mystl::copy
+      std::copy(last, finish, first);
+      iterator new_finish = finish - n;
+      destroy(new_finish, finish);
+      for(map_pointer cur = new_finish.node + 1; cur <= finish.node; ++cur)
+      {
+        data_allocator::deallocate(*cur, buffer_size());
+      }
+      finish = new_finish;
+    }
+    return start + elems_before;
+  }
+}
+
+template <class T, class Alloc, size_t BufSize>
+typename deque<T, Alloc, BufSize>::iterator
+deque<T, Alloc, BufSize>::insert_aux(iterator pos, const value_type &x)
+{
+  difference_type index = pos - start;
+  value_type x_copy = x;
+  if(index < size() / 2)
+  {
+    push_front(front());
+    iterator front1 = start;
+    ++front1;
+    iterator front2 = front1;
+    ++front2;
+    pos = start + index;
+    iterator pos1 = pos;
+    ++pos1;
+    //TODO: will be changed to mystl::copy
+    std::copy(front2, pos1, front1);
+  }
+  else
+  {
+    push_back(back());
+    iterator back1 = finish;
+    --back1;
+    iterator back2 = back1;
+    --back2;
+    pos = start + index;
+    //TODO: will be changed to mystl::copy_backward
+    std::copy_backward(pos, back2, back1);
+  }
+  *pos = x_copy;
+  return pos;
 }
 
 } // end of namespace stl
--- test/test_deque.cpp
+++ test/test_deque.cpp
@@ -37,6 +37,74 @@ int main()
   std::cout << "elements in d2: ";
   print_elements(d2);
 
+  // test clear
+  d1.clear();
+  std::cout << "elements in d1 after clear(): ";
+  print_elements(d1);
+
+  // test pop_back
+  for(int i = 0; i != 10; ++i)
+  {
+    d2.pop_back();
+  }
+  std::cout << "elements in d2: ";
+  print_elements(d2);
+
+  // test pop_front
+  for(int i = 0; i != 10; ++i)
+  {
+    d2.pop_front();
+  }
+  std::cout << "elements in d2: ";
+  print_elements(d2);
+
+  // test erase 1
+  d2.clear();
+  for(int i = 0; i != 10; ++i)
+  {
+    d2.push_back(i);
+  }
+  std::cout << "elements in d2 before erase: ";
+  print_elements(d2);
+  mystl::deque<int, mystl::alloc, 8>::iterator iter;
+  iter = d2.begin();
+  d2.erase(iter);
+  std::cout << "elements in d2 after erase: ";
+  print_elements(d2);
+
+  // test erase 2
+  iter = d2.begin();
+  for(int i = 0; i != 5; ++i)
+  {
+    d2.erase(++iter);
+  }
+  std::cout << "elements in d2 after erase: ";
+  print_elements(d2);
+
+  // test erase 3
+  mystl::deque<int, mystl::alloc, 8>::iterator start = d2.begin();
+  mystl::deque<int, mystl::alloc, 8>::iterator finish = d2.end();
+  ++start;
+  --finish;
+  d2.erase(start, finish);
+  std::cout << "elements in d2 after erase: ";
+  print_elements(d2);
+
+  // test erase 4
+  start = d2.begin();
+  finish = d2.end();
+  d2.erase(start, finish);
+  std::cout << "elements in d2 after erase: ";
+  print_elements(d2);
+
+  // test insert
+  d2.insert(d2.end(), 1);
+  d2.insert(d2.begin(), 0);
+  iter = d2.begin();
+  d2.insert(++iter, 2);
+  std::cout << "elements in d2 after insert: ";
+  print_elements(d2);
+
   return 0;
 }
 
