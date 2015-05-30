074dcf8274673076fc8aa77ebb5f3e940f21c139
Mengjiao Wang <wmjdut@gmail.com>
2015-05-30 13:04:36 +0800

    fix bug in stack test

--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -20,16 +20,16 @@ int main()
   // use mystl::list as the container for stack
   mystl::stack<int, mystl::list<int> > sl;
   // test push back
-  s.push(1);
-  s.push(2);
-  s.push(3);
+  sl.push(1);
+  sl.push(2);
+  sl.push(3);
   std::cout << "size = " << s.size() << std::endl;
 
   // test pop
-  std::cout << "s.pop() = " << s.top() << std::endl; s.pop();
-  std::cout << "s.pop() = " << s.top() << std::endl; s.pop();
-  std::cout << "s.pop() = " << s.top() << std::endl; s.pop();
-  std::cout << "size = " << s.size() << std::endl;
+  std::cout << "s.pop() = " << sl.top() << std::endl; sl.pop();
+  std::cout << "s.pop() = " << sl.top() << std::endl; sl.pop();
+  std::cout << "s.pop() = " << sl.top() << std::endl; sl.pop();
+  std::cout << "size = " << sl.size() << std::endl;
 
 
   return 0;
