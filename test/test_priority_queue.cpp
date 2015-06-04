90fadeba86b76274355082f7b7bc97b333335548
Mengjiao Wang <wmjdut@gmail.com>
2015-06-04 21:45:34 +0800

    add more test to priority queue

--- test/test_priority_queue.cpp
+++ test/test_priority_queue.cpp
@@ -13,6 +13,19 @@ int main()
   pq.push(5);
   // test size
   std::cout << "size = " << pq.size() << std::endl;
+  // test top
+  std::cout << pq.top() << " " << std::endl;
+  // test pop
+  pq.pop();
+  std::cout << "size = " << pq.size() << std::endl;
+
+  int size = pq.size();
+  for(int i = 0; i != size; ++i)
+  {
+    std::cout << pq.top() << " ";
+    pq.pop();
+  }
+  std::cout << std::endl;
 
   return 0;
 }
