f108061b85c114f2ade1880d232afa046b673945
Mengjiao Wang <wmjdut@gmail.com>
2015-06-04 12:38:35 +0800

    enable compare function in heap

--- mystl_heap.h
+++ mystl_heap.h
@@ -52,6 +52,20 @@ namespace mystl
     *(first + holeIndex) = value;
   }
 
+  template <class RandomAccessIterator, class Compare, class Distance, class T>
+  void __push_heap(RandomAccessIterator first, Distance holeIndex,
+      Distance topIndex, T value, Compare comp)
+  {
+    Distance parent = (holeIndex - 1) / 2;
+    while((holeIndex > topIndex) && comp(*(first + parent), value))
+    {
+      *(first + holeIndex) = *(first + parent);
+      holeIndex = parent;
+      parent = (holeIndex - 1) / 2;
+    }
+    *(first + holeIndex) = value;
+  }
+
   template <class RandomAccessIterator, class Distance, class T>
   inline void __push_heap_aux(RandomAccessIterator first,
       RandomAccessIterator last, Distance*, T*)
@@ -59,10 +73,24 @@ namespace mystl
     __push_heap(first, Distance((last - first) - 1), Distance(0), T(*(last - 1)));
   }
 
+  template <class RandomAccessIterator, class Compare, class Distance, class T>
+  inline void __push_heap_aux(RandomAccessIterator first,
+      RandomAccessIterator last, Compare comp, Distance*, T*)
+  {
+    __push_heap(first, Distance((last - first) - 1), Distance(0), T(*(last - 1)), comp);
+  }
+
   template <class RandomAccessIterator>
   inline void push_heap(RandomAccessIterator first, RandomAccessIterator last)
   {
     __push_heap_aux(first, last, distance_type(first), value_type(first));
+  }
+
+  template <class RandomAccessIterator, class Compare>
+  inline void push_heap(RandomAccessIterator first, RandomAccessIterator last,
+      Compare comp)
+  {
+    __push_heap_aux(first, last, comp, distance_type(first), value_type(first));
   }
 
   template <class RandomAccessIterator, class Distance, class T>
@@ -89,6 +117,32 @@ namespace mystl
       holeIndex = secondChild - 1;
     }
     __push_heap(first, holeIndex, topIndex, value);
+  }
+
+  template <class RandomAccessIterator, class Compare, class Distance, class T>
+  void __adjust_heap(RandomAccessIterator first, Distance holeIndex,
+      Distance len, T value, Compare comp)
+  {
+    Distance topIndex = holeIndex;
+    Distance secondChild = 2 * holeIndex + 2; // right child
+    while(secondChild < len)
+    {
+      if(comp(*(first + secondChild), *(first + (secondChild - 1))))
+      {
+        --secondChild;
+      }
+      // percolate down
+      *(first + holeIndex) = *(first + secondChild);
+      holeIndex = secondChild;
+      secondChild = 2 * (secondChild + 1);
+    }
+    if(secondChild == len)
+    {
+      // percolate down
+      *(first + holeIndex) = *(first + (secondChild - 1));
+      holeIndex = secondChild - 1;
+    }
+    __push_heap(first, holeIndex, topIndex, value, comp);
   }
 
   template <class RandomAccessIterator, class Distance, class T>
@@ -138,10 +192,33 @@ namespace mystl
     }
   }
 
+  template <class RandomAccessIterator, class Compare, class T, class Distance>
+  void __make_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp,
+      T*, Distance*)
+  {
+    if(last - first < 2) return;
+    Distance len = last - first;
+    Distance parent = (len - 2) / 2;
+    while(true)
+    {
+      __adjust_heap(first, parent, len, T(*(first + parent)), comp);
+      if(parent == 0) return;
+      --parent;
+    }
+  }
+
   template <class RandomAccessIterator>
   inline void make_heap(RandomAccessIterator first, RandomAccessIterator last)
   {
     __make_heap(first, last, value_type(first), distance_type(first));
+  }
+
+
+  template <class RandomAccessIterator, class Compare>
+  inline void make_heap(RandomAccessIterator first, RandomAccessIterator last,
+      Compare comp)
+  {
+    __make_heap(first, last, comp, value_type(first), distance_type(first));
   }
 
 }// end of namespace mystl
