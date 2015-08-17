ecf4f13b8aa90fb73c73d63ac0e01adad80ccabf
Mengjiao Wang <wmjdut@gmail.com>
2015-08-17 22:19:10 +0800

    enhance hashtable

--- mystl_hashtable.h
+++ mystl_hashtable.h
@@ -123,6 +123,67 @@ namespace mystl
     return tmp;
   }
 
+  template <class Value, class Key, class HashFcn,
+            class ExtractKey, class EqualKey, class Alloc>
+  struct __hashtable_const_iterator
+  {
+    typedef hashtable<Value, Key, HashFcn, ExtractKey,
+                    EqualKey, Alloc> hashtable;
+    typedef __hashtable_iterator<Value, Key, HashFcn, ExtractKey,
+                    EqualKey, Alloc> iterator;
+    typedef __hashtable_const_iterator<Value, Key, HashFcn, ExtractKey,
+                    EqualKey, Alloc> const_iterator;
+    typedef __hashtable_node<Value> node;
+
+    typedef forward_iterator_tag iterator_category;
+    typedef Value value_type;
+    typedef ptrdiff_t difference_type;
+    typedef size_t size_type;
+    typedef Value& reference;
+    typedef Value* pointer;
+
+    node *cur;
+    hashtable *ht;
+
+    __hashtable_const_iterator(node *n, hashtable *tab): cur(n), ht(tab){}
+    __hashtable_const_iterator(){}
+    __hashtable_const_iterator(const iterator& it) : cur(it.cur), ht(it.ht) {}
+    reference operator*() const {return cur->val;}
+    pointer operator->() const {return &(operator*());}
+    const_iterator& operator++();
+    const_iterator operator++(int);
+    bool operator==(const const_iterator &it) const {return cur == it.cur;}
+    bool operator!=(const const_iterator &it) const {return cur != it.cur;}
+  };
+
+  template <class Value, class Key, class HashFcn,
+            class ExtractKey, class EqualKey, class Alloc>
+  __hashtable_const_iterator<Value, Key, HashFcn, ExtractKey, EqualKey, Alloc> &
+  __hashtable_const_iterator<Value, Key, HashFcn, ExtractKey, EqualKey, Alloc>::operator++()
+  {
+    const node* old = cur;
+    cur = cur->next;
+    if(!cur)
+    {
+      size_type bucket = ht->bkt_num(old->val);
+      while(!cur && ++bucket < ht->buckets.size())
+      {
+        cur = ht->buckets[bucket];
+      }
+    }
+    return *this;
+  }
+
+  template <class Value, class Key, class HashFcn,
+            class ExtractKey, class EqualKey, class Alloc>
+  __hashtable_const_iterator<Value, Key, HashFcn, ExtractKey, EqualKey, Alloc>
+  __hashtable_const_iterator<Value, Key, HashFcn, ExtractKey, EqualKey, Alloc>::operator++(int)
+  {
+    iterator tmp = *this;
+    ++*this;
+    return tmp;
+  }
+
   static const int __mystl_num_primes = 28;
   static const unsigned long __mystl_prime_list[__mystl_num_primes] =
   {
@@ -310,6 +371,10 @@ namespace mystl
         }
         return result;
       }
+
+      size_type size() const {return num_elements;}
+      size_type max_size() const {return size_type(-1);}
+      bool empty() const {return size() == 0;}
 
     public:
       // data access
