125fd852440ae1162ac25c0412302f4442fb1d45
Mengjiao Wang <wmjdut@gmail.com>
2015-08-25 18:32:43 +0800

    enhance mystl functions

--- mystl_function.h
+++ mystl_function.h
@@ -38,30 +38,163 @@
 
 namespace mystl
 {
+
+  template <class Arg, class Result>
+  struct unary_function
+  {
+    typedef Arg argument_type;
+    typedef Result result_type;
+  };
+
+  template <class Arg1, class Arg2, class Result>
+  struct binary_function
+  {
+    typedef Arg1 first_argument_type;
+    typedef Arg2 second_argument_type;
+    typedef Result result_type;
+  };
+
   template <class T>
-  //TODO: will be changed to mystl::unary_function<T, T>
-  struct multiplies : public std::binary_function<T, T, T>
+  struct plus: public binary_function<T, T, T>
+  {
+    T operator()(const T& x, const T& y) const {return x + y;}
+  };
+
+  template <class T>
+  struct minus: public binary_function<T, T, T>
+  {
+    T operator()(const T& x, const T& y) const {return x - y;}
+  };
+
+  template <class T>
+  struct multiplies : public binary_function<T, T, T>
   {
     T operator()(const T& x, const T& y) const { return x * y;}
   };
 
   template <class T>
-  struct plus : public std::binary_function<T, T, T> {
-    T operator()(const T& x, const T& y) const { return x + y;}
+  struct devide: public binary_function<T, T, T>
+  {
+    T operator()(const T& x, const T& y) const { return x / y;}
   };
 
   template <class T> inline T identity_element(mystl::multiplies<T>) { return T(1);}
   template <class T> inline T identity_element(mystl::plus<T>) { return T(0);}
 
   template <class T>
-  //TODO: will be changed to mystl::unary_function<T, T>
-  struct identity: public std::unary_function<T, T>
+  struct modulus: public binary_function<T, T, T>
+  {
+    T operator()(const T& x, const T& y) {return x % y;}
+  };
+
+  template <class T>
+  struct negate: public unary_function<T, T>
+  {
+    T operator()(const T& x) const {return -x;}
+  };
+
+  template <class T>
+  struct equal_to: public binary_function<T, T, T>
+  {
+    T operator()(const T& x, const T& y) {return x == y;}
+  };
+
+  template <class T>
+  struct not_equal_to: public binary_function<T, T, T>
+  {
+    T operator()(const T& x, const T& y) {return x != y;}
+  };
+
+  template <class T>
+  struct greater: public binary_function<T, T, T>
+  {
+    T operator()(const T& x, const T& y) {return x > y;}
+  };
+
+  template <class T>
+  struct less: public binary_function<T, T, T>
+  {
+    T operator()(const T& x, const T& y) {return x < y;}
+  };
+
+  template <class T>
+  struct greater_equal: public binary_function<T, T, T>
+  {
+    T operator()(const T& x, const T& y) {return x >= y;}
+  };
+
+  template <class T>
+  struct less_equal: public binary_function<T, T, T>
+  {
+    T operator()(const T& x, const T& y) {return x <= y;}
+  };
+
+  template <class T>
+  struct logical_and: public binary_function<T, T, T>
+  {
+    T operator()(const T& x, const T& y) {return x && y;}
+  };
+
+  template <class T>
+  struct logical_or: public binary_function<T, T, T>
+  {
+    T operator()(const T& x, const T& y) {return x || y;}
+  };
+
+  template <class T>
+  struct logical_not: public unary_function<T, T>
+  {
+    T operator()(const T& x) {return !x;}
+  };
+
+  template <class Predicate>
+  class unary_negate: public unary_function<typename Predicate::argument_type, bool>
+  {
+    protected:
+      Predicate pred;
+    public:
+      explicit unary_negate(const Predicate &x): pred(x) {}
+      bool operator()(const typename Predicate::argument_type &x) const
+      {
+        return !pred(x);
+      }
+  };
+
+  template <class Predicate>
+  inline unary_negate<Predicate> no1(const Predicate &pred)
+  {
+    return unary_negate<Predicate>(pred);
+  }
+
+  template <class Predicate>
+  class binary_negate: public binary_function<typename Predicate::argument_type,
+    typename Predicate::argument_type, bool>
+  {
+    protected:
+      Predicate pred;
+    public:
+      explicit binary_negate(const Predicate &x): pred(x) {}
+      bool operator()(const typename Predicate::argument_type &x,
+          const typename Predicate::argument_type &y) const
+      {
+        return !pred(x, y);
+      }
+  };
+
+  template <class Predicate>
+  inline binary_negate<Predicate> no1(const Predicate &pred)
+  {
+    return binary_negate<Predicate>(pred);
+  }
+
+  template <class T>
+  struct identity: public unary_function<T, T>
   {
     const T& operator()(const T& x) const {return x;}
   };
 
   template <class Pair>
-  struct select1st : public std::unary_function<Pair, typename Pair::first_type>
+  struct select1st : public unary_function<Pair, typename Pair::first_type>
   {
     const typename Pair::first_type& operator()(const Pair& x) const
     {
